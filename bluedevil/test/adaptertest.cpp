81ad06d65288ea9fb7490799f70ca72f883fad4a
Rafael Fernández López <ereslibre@ereslibre.es>
2011-03-04 13:27:46 +0100

    Add another test that will loop checking information provided by libbluedevil

---
+++ bluedevil/test/adaptertest.cpp
@@ -0,0 +1,111 @@
+/*****************************************************************************
+ * This file is part of the BlueDevil project                                *
+ *                                                                           *
+ * Copyright (C) 2011 Rafael Fernández López <ereslibre@kde.org>             *
+ * Copyright (C) 2011 UFO Coders <info@ufocoders.com>                        *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or             *
+ * modify it under the terms of the GNU Library General Public               *
+ * License as published by the Free Software Foundation; either              *
+ * version 2 of the License, or (at your option) any later version.          *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public License *
+ * along with this library; see the file COPYING.LIB.  If not, write to      *
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,      *
+ * Boston, MA 02110-1301, USA.                                               *
+ *****************************************************************************/
+
+#include "adaptertest.h"
+
+#include <QtCore/QDebug>
+#include <QtGui/QApplication>
+
+#include <bluedevil/bluedeviladapter.h>
+#include <bluedevil/bluedevilmanager.h>
+#include <bluedevil/bluedevildevice.h>
+
+using namespace BlueDevil;
+
+AdapterTest::AdapterTest(QObject *parent)
+    : QThread(parent)
+{
+}
+
+AdapterTest::~AdapterTest()
+{
+}
+
+void AdapterTest::adapterAdded(Adapter *adapter)
+{
+    qDebug() << "Adapter added: " << adapter;
+    Manager *const manager = Manager::self();
+    qDebug() << "\tBluetooth Operational: " << manager->isBluetoothOperational();
+    qDebug() << "\tDefault Adapter: " << manager->defaultAdapter();
+    qDebug() << "\tUsable Adapter: " << manager->usableAdapter();
+}
+
+void AdapterTest::adapterRemoved(Adapter *adapter)
+{
+    qDebug() << "Adapter removed: " << adapter;
+    Manager *const manager = Manager::self();
+    qDebug() << "\tBluetooth Operational: " << manager->isBluetoothOperational();
+    qDebug() << "\tDefault Adapter: " << manager->defaultAdapter();
+    qDebug() << "\tUsable Adapter: " << manager->usableAdapter();
+}
+
+void AdapterTest::defaultAdapterChanged(Adapter *adapter)
+{
+    qDebug() << "Default adapter changed: " << adapter;
+    Manager *const manager = Manager::self();
+    qDebug() << "\tBluetooth Operational: " << manager->isBluetoothOperational();
+    qDebug() << "\tDefault Adapter: " << manager->defaultAdapter();
+    qDebug() << "\tUsable Adapter: " << manager->usableAdapter();
+}
+
+void AdapterTest::usableAdapterChanged(Adapter *adapter)
+{
+    qDebug() << "Usable adapter changed: " << adapter;
+    Manager *const manager = Manager::self();
+    qDebug() << "\tBluetooth Operational: " << manager->isBluetoothOperational();
+    qDebug() << "\tDefault Adapter: " << manager->defaultAdapter();
+    qDebug() << "\tUsable Adapter: " << manager->usableAdapter();
+}
+
+void AdapterTest::allAdaptersRemoved()
+{
+    qDebug() << "All adapters have been removed";
+    Manager *const manager = Manager::self();
+    qDebug() << "\tBluetooth Operational: " << manager->isBluetoothOperational();
+    qDebug() << "\tDefault Adapter: " << manager->defaultAdapter();
+    qDebug() << "\tUsable Adapter: " << manager->usableAdapter();
+}
+
+void AdapterTest::run()
+{
+    Manager *const manager = Manager::self();
+    while (true) {
+        qDebug() << "Bluetooth Operational: " << manager->isBluetoothOperational();
+        qDebug() << "Default Adapter: " << manager->defaultAdapter();
+        qDebug() << "Usable Adapter: " << manager->usableAdapter();
+        sleep(5);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+
+    qDebug() << "Looping until stopped";
+
+    AdapterTest *const adapterTest = new AdapterTest;
+    adapterTest->start();
+
+    return app.exec();
+}
+
+#include "adaptertest.moc"
